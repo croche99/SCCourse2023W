bdbf69b6b5e59898a4db15c7d360fc957657f7ed
croche99 <155962827+croche99@users.noreply.github.com>
2024-01-26 22:58:12 +0100

    Update trimming.sh

--- trimming.sh
+++ trimming.sh
@@ -16,4 +16,10 @@ module list
 cd /scratch/course/2023w300106/rochearcas/SRR24348405
 
 ##ACTUAL COMMAND
-trimmomatic PE -threads 8 SRR24348405_1.fastq SRR24348405_2.fastq SRR24348405_R1.fastq SRR24348405_S1.fastq SRR24348405_R2.fastq SRR24348405_S2.fastq ILLUMINACLIP:/scratch/course/2023w300106/rochearcas/adapters.fa:2:30:10 SLIDINGWINDOW:6:15 MINLEN:75
+trimmomatic PE -threads 8 \
+              SRR24348405_1.fastq SRR24348405_2.fastq \ #Input fastq reads for the trimming
+              SRR24348405_R1.fastq SRR24348405_S1.fastq \ #Output files for read 1
+              SRR24348405_R2.fastq SRR24348405_S2.fastq \ #OUtput files for read 2
+              ILLUMINACLIP:/scratch/course/2023w300106/rochearcas/adapters.fa:2:30:10 \ #Identify adapter sequence files
+              SLIDINGWINDOW:6:15 \ #In a sliding windoe of 6 bp, if the quality falls under 15, the read is discarded
+              MINLEN:75
