faf3914459b6fd0736536842bd18dbda04fbbfb7
croche99 <155962827+croche99@users.noreply.github.com>
2024-01-26 23:00:11 +0100

    Update STAR_trimmed.sh

--- STAR_trimmed.sh
+++ STAR_trimmed.sh
@@ -17,7 +17,11 @@ cd /scratch/course/2023w300106/rochearcas/alignments/trimmed/
 
 
 ##script for STAR
-STAR --runThreadN 16 --genomeDir /scratch/course/2023w300106/rochearcas/genome/ncbi_dataset/data/GCF_932526225.1/jaNemVect1.1_STAR \
-	--readFilesIn /scratch/course/2023w300106/rochearcas/rawreads/trimmed_reads/SRR24348405_R1.fastq /scratch/course/2023w300106/rochearcas/rawreads/trimmed_reads/SRR24348405_R2.fastq \
-	--outSAMtype BAM SortedByCoordinate --twopassMode Basic \
-	--outSAMstrandField intronMotif --outFileNamePrefix SRR24348405_trimmed
+STAR --runThreadN 16 \
+	--genomeDir /scratch/course/2023w300106/rochearcas/genome/ncbi_dataset/data/GCF_932526225.1/jaNemVect1.1_STAR \ #Genome needs to be previously indexed
+	--readFilesIn /scratch/course/2023w300106/rochearcas/rawreads/trimmed_reads/SRR24348405_R1.fastq \
+ 		      /scratch/course/2023w300106/rochearcas/rawreads/trimmed_reads/SRR24348405_R2.fastq \
+	--outSAMtype BAM SortedByCoordinate \
+ 	--twopassMode Basic \
+	--outSAMstrandField intronMotif \
+ 	--outFileNamePrefix SRR24348405_trimmed
